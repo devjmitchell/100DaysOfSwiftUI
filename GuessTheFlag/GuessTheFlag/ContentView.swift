1de9c91ddaa19ae327980b7733307f2bae3d5a82
Jason Mitchell <devjmitchell@gmail.com>
2019-10-29 10:28:56 -0700

    Complete Day 20
    - Create GuessTheFlag project
    - Also learned some new SwiftUI features, but they're deleted to get project back to "starting point" for tutorials starting on next day

---
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Jason Mitchell on 10/29/19.
+//  Copyright © 2019 Jason Mitchell. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var showingAlert = false
+    
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
