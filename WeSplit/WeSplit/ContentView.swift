0d4c04bc99ce9d2768441fb2e74f91bf94437467
Jason Mitchell <devjmitchell@gmail.com>
2019-10-31 10:04:49 -0700

    Day 24 - complete challenge 2

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -54,6 +54,7 @@ struct ContentView: View {
                 
                 Section(header: Text("Total")) {
                     Text("$\(grandTotal, specifier: "%.2f")")
+                        .foregroundColor(tipPercentages[tipPercentage] == 0 ? .red : .primary)
                 }
                 
                 Section(header: Text("Amount per person")) {
