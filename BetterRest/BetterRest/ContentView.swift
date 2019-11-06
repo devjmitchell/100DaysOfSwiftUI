207971e6fe5929ff6e3c4fee5e04d9eb3f0f7f6f
Jason Mitchell <devjmitchell@gmail.com>
2019-11-06 09:44:40 -0700

    Day 26 - Create BetterRest project

---
+++ BetterRest/BetterRest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Jason Mitchell on 11/6/19.
+//  Copyright © 2019 Jason Mitchell. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
