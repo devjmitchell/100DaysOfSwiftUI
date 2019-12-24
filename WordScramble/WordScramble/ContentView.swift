138632f998915f912ec212cad817672e3ff3e18d
Jason Mitchell <devjmitchell@gmail.com>
2019-12-24 10:17:20 -0700

    Day 29 - create empty project for WordScramble

---
+++ WordScramble/WordScramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Jason Mitchell on 12/24/19.
+//  Copyright © 2019 Jason Mitchell. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
