6fb34a7970653dc84c10f5e99e6d64ef694a1778
Jason Mitchell <devjmitchell@gmail.com>
2019-12-26 12:11:07 -0700

    Create cool expanding overlay animation on a button

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -13,23 +13,25 @@ struct ContentView: View {
     
     var body: some View {
         Button("Tap Me") {
-            self.animationAmount += 1
+//            self.animationAmount += 1
         }
         .padding(50)
         .background(Color.red)
         .foregroundColor(.white)
         .clipShape(Circle())
-        .scaleEffect(animationAmount)
-        .blur(radius: (animationAmount - 1) * 3)
-//        .animation(.default)
-//        .animation(.interpolatingSpring(stiffness: 50, damping: 1))
-//        .animation(.easeInOut(duration: 2))
-        .animation(
-            Animation.easeInOut(duration: 1)
-//                .delay(1)
-//                .repeatCount(3, autoreverses: true)
-                .repeatForever(autoreverses: true)
+        .overlay(
+            Circle()
+                .stroke(Color.red)
+                .scaleEffect(animationAmount)
+                .opacity(Double(2 - animationAmount))
+                .animation(
+                    Animation.easeInOut(duration: 1)
+                        .repeatForever(autoreverses: false)
+            )
         )
+            .onAppear {
+                self.animationAmount = 2
+        }
     }
 }
 
