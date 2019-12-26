4fd265904354da8dc1b30f0e0adf486c7081eac4
Jason Mitchell <devjmitchell@gmail.com>
2019-12-26 11:46:52 -0700

    Day 32 - create Animations project

---
+++ Animations/Animations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by Jason Mitchell on 12/26/19.
+//  Copyright © 2019 Jason Mitchell. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
