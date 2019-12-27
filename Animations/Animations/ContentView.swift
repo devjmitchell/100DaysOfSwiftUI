cf91c23d7242f1dc491be4549f1b4489ca0962c0
Jason Mitchell <devjmitchell@gmail.com>
2019-12-27 09:39:53 -0700

    Day 33 - Animate LinearGradient

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -9,19 +9,22 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
     
     var body: some View {
-        Button("Tap Me") {
-            self.enabled.toggle()
-        }
-        .frame(width: 200, height: 200)
-        .background(enabled ? Color.blue : Color.red)
-//        .animation(.default)
-        .animation(nil)
-        .foregroundColor(.white)
-        .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
-        .animation(.interpolatingSpring(stiffness: 10, damping: 1))
+        LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            .frame(width: 300, height: 200)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .offset(dragAmount)
+            .gesture(
+                DragGesture()
+                    .onChanged { self.dragAmount = $0.translation }
+                    .onEnded { _ in
+                        withAnimation(.spring()) {
+                            self.dragAmount = .zero
+                        }
+                }
+        )
     }
 }
 
