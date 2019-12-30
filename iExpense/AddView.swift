88ae810207b48d895c03f3870a46c898a688b85f
Jason Mitchell <devjmitchell@gmail.com>
2019-12-30 11:46:54 -0700

    Day 37 - final polish on iExpense following tutorial

--- iExpense/AddView.swift
+++ iExpense/AddView.swift
@@ -9,6 +9,7 @@
 import SwiftUI
 
 struct AddView: View {
+    @Environment(\.presentationMode) var presentationMode
     @ObservedObject var expenses: Expenses
     @State private var name = ""
     @State private var type = "Personal"
@@ -35,6 +36,7 @@ struct AddView: View {
                 if let actualAmount = Int(self.amount) {
                     let item = ExpenseItem(name: self.name, type: self.type, amount: actualAmount)
                     self.expenses.items.append(item)
+                    self.presentationMode.wrappedValue.dismiss()
                 }
             })
         }
