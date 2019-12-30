afeb33cbf5aeaeaf6acf9996bf1938ac04674f66
Jason Mitchell <devjmitchell@gmail.com>
2019-12-30 11:41:39 -0700

    Day 37 - sharing an observed object with a new view and making changes permanent with UserDefaults

---
+++ iExpense/AddView.swift
@@ -0,0 +1,48 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Jason Mitchell on 12/30/19.
+//  Copyright © 2019 Jason Mitchell. All rights reserved.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @ObservedObject var expenses: Expenses
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = ""
+    static let types = ["Business", "Personal"]
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                TextField("Name", text: $name)
+                
+                Picker("Type", selection: $type) {
+                    ForEach(Self.types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                
+                
+                TextField("Amount", text: $amount)
+                    .keyboardType(.numberPad)
+            }
+            .navigationBarTitle("Add new expense")
+            .navigationBarItems(trailing: Button("Save") {
+                if let actualAmount = Int(self.amount) {
+                    let item = ExpenseItem(name: self.name, type: self.type, amount: actualAmount)
+                    self.expenses.items.append(item)
+                }
+            })
+        }
+    }
+}
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddView(expenses: Expenses())
+    }
+}
