dc236b0df2500af802bffc30a4bacb64dca02a25
Jason Mitchell <devjmitchell@gmail.com>
2020-02-22 19:25:46 -0700

    Day 40 - decode missions and display in list

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Jason Mitchell on 2/22/20.
+//  Copyright © 2020 Jason Mitchell. All rights reserved.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
