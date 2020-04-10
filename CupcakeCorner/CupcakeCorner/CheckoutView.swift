97bc459f030a235bf2ab6bf3d39fc0dfaa7036bc
Jason Mitchell <devjmitchell@gmail.com>
2020-04-10 16:47:05 -0700

    Day 50 - adding more screens

---
+++ CupcakeCorner/CupcakeCorner/CheckoutView.swift
@@ -0,0 +1,41 @@
+//
+//  CheckoutView.swift
+//  CupcakeCorner
+//
+//  Created by Jason Mitchell on 4/10/20.
+//  Copyright © 2020 Jason Mitchell. All rights reserved.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    @ObservedObject var order: Order
+    
+    var body: some View {
+        GeometryReader { geo in
+            ScrollView {
+                VStack {
+                    Image("cupcakes")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: geo.size.width)
+                    
+                    Text("Your total is $\(self.order.cost, specifier: "%.2f")")
+                        .font(.title)
+                    
+                    Button("Place order") {
+                        // place the order
+                    }
+                    .padding()
+                }
+            }
+        }
+        .navigationBarTitle("Check out", displayMode: .inline)
+    }
+}
+
+struct CheckoutView_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckoutView(order: Order())
+    }
+}
