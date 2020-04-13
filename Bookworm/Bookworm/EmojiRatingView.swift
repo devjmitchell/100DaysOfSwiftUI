863792e6b90d548934afdc0aa9462b1a83a130f8
Jason Mitchell <devjmitchell@gmail.com>
2020-04-13 12:49:37 -0700

    Day 54 - create first part of Bookworm app using Core Data and a custom RatingView

---
+++ Bookworm/Bookworm/EmojiRatingView.swift
@@ -0,0 +1,34 @@
+//
+//  EmojiRatingView.swift
+//  Bookworm
+//
+//  Created by Jason Mitchell on 4/13/20.
+//  Copyright © 2020 Jason Mitchell. All rights reserved.
+//
+
+import SwiftUI
+
+struct EmojiRatingView: View {
+    let rating: Int16
+    
+    var body: some View {
+        switch rating {
+        case 1:
+            return Text("😴")
+        case 2:
+            return Text("☹️")
+        case 3:
+            return Text("😐")
+        case 4:
+            return Text("😄")
+        default:
+            return Text("🤩")
+        }
+    }
+}
+
+struct EmojiRatingView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmojiRatingView(rating: 3)
+    }
+}
