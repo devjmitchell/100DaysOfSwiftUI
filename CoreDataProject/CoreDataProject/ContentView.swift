dd93e067987d350f821e60a661cf045dccf0f1a4
Jason Mitchell <devjmitchell@gmail.com>
2020-04-25 08:56:17 -0700

    Day 57 - Ensuring Core Data objects are unique using constraints

--- CoreDataProject/CoreDataProject/ContentView.swift
+++ CoreDataProject/CoreDataProject/ContentView.swift
@@ -9,8 +9,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(entity: Wizard.entity(), sortDescriptors: []) var wizards: FetchedResults<Wizard>
+    
     var body: some View {
-        Text("Hello World")
+        VStack {
+            List(wizards, id: \.self) { wizard in
+                Text(wizard.name ?? "Unknown")
+            }
+            
+            Button("Add") {
+                let wizard = Wizard(context: self.moc)
+                wizard.name = "Harry Potter"
+            }
+            
+            Button("Save") {
+                do {
+                    try self.moc.save()
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+        }
     }
 }
 
