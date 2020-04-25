093fa23ee795638c917ccb71d61fdb2fde247151
Jason Mitchell <devjmitchell@gmail.com>
2020-04-25 08:47:49 -0700

    Day 57 - Conditional saving of NSManagedObjectContext

--- CoreDataProject/CoreDataProject/ContentView.swift
+++ CoreDataProject/CoreDataProject/ContentView.swift
@@ -9,8 +9,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) var moc
+    
     var body: some View {
-        Text("Hello, World!")
+        Button("Save") {
+            if self.moc.hasChanges {
+                try? self.moc.save()
+            }
+        }
     }
 }
 
