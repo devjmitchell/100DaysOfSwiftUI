b81a1544dc66825bc697f67198311a8a8a49f656
Jason Mitchell <devjmitchell@gmail.com>
2020-04-25 08:28:48 -0700

    Day 57 - create new project for CoreDataProject

---
+++ CoreDataProject/CoreDataProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoreDataProject
+//
+//  Created by Jason Mitchell on 4/25/20.
+//  Copyright © 2020 Jason Mitchell. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
