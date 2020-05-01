bf62a47e2c0de740845dfc94fff8d15dbdb1d3c0
Jason Mitchell <devjmitchell@gmail.com>
2020-05-01 10:34:08 -0700

    Day 58 - more Dynamically filtering @FetchRequest with SwiftUI

--- CoreDataProject/CoreDataProject/ContentView.swift
+++ CoreDataProject/CoreDataProject/ContentView.swift
@@ -15,7 +15,9 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            FilteredList(filter: lastNameFilter)
+            FilteredList(filterKey: "lastName", filterValue: lastNameFilter) { (singer: Singer) in
+                Text("\(singer.wrappedFirstName) \(singer.wrappedLastName)")
+            }
             
             Button("Add Examples") {
                 let taylor = Singer(context: self.moc)
