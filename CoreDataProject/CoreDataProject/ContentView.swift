9a71ec6a1266c345f9cc8279cc9435c8acd05624
Jason Mitchell <devjmitchell@gmail.com>
2020-05-01 10:09:33 -0700

    Day 58 - Filtering @FetchRequest using NSPredicate

--- CoreDataProject/CoreDataProject/ContentView.swift
+++ CoreDataProject/CoreDataProject/ContentView.swift
@@ -6,29 +6,46 @@
 //  Copyright © 2020 Jason Mitchell. All rights reserved.
 //
 
+import CoreData
 import SwiftUI
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
-    @FetchRequest(entity: Wizard.entity(), sortDescriptors: []) var wizards: FetchedResults<Wizard>
+//    @FetchRequest(entity: Ship.entity(), sortDescriptors: [], predicate: NSPredicate(format: "universe == 'Star Wars'")) var ships: FetchedResults<Ship>
+//    @FetchRequest(entity: Ship.entity(), sortDescriptors: [], predicate: NSPredicate(format: "universe == %@", "Star Wars")) var ships: FetchedResults<Ship>
+//    @FetchRequest(entity: Ship.entity(), sortDescriptors: [], predicate: NSPredicate(format: "name < %@", "F")) var ships: FetchedResults<Ship>
+//    @FetchRequest(entity: Ship.entity(), sortDescriptors: [], predicate: NSPredicate(format: "universe in %@", ["Aliens", "Firefly", "Star Trek"])) var ships: FetchedResults<Ship>
+//    @FetchRequest(entity: Ship.entity(), sortDescriptors: [], predicate: NSPredicate(format: "name BEGINSWITH[c] %@", "e")) var ships: FetchedResults<Ship>
+    @FetchRequest(entity: Ship.entity(), sortDescriptors: [], predicate: NSPredicate(format: "NOT name BEGINSWITH[c] %@", "e")) var ships: FetchedResults<Ship>
+
+
+
+
     
     var body: some View {
         VStack {
-            List(wizards, id: \.self) { wizard in
-                Text(wizard.name ?? "Unknown")
+            List(ships, id: \.self) { ship in
+                Text(ship.name ?? "Unknown name")
             }
             
-            Button("Add") {
-                let wizard = Wizard(context: self.moc)
-                wizard.name = "Harry Potter"
-            }
-            
-            Button("Save") {
-                do {
-                    try self.moc.save()
-                } catch {
-                    print(error.localizedDescription)
-                }
+            Button("Add Examples") {
+                let ship1 = Ship(context: self.moc)
+                ship1.name = "Enterprise"
+                ship1.universe = "Star Trek"
+                
+                let ship2 = Ship(context: self.moc)
+                ship2.name = "Defiant"
+                ship2.universe = "Star Trek"
+                
+                let ship3 = Ship(context: self.moc)
+                ship3.name = "Millennium Falcon"
+                ship3.universe = "Star Wars"
+                
+                let ship4 = Ship(context: self.moc)
+                ship4.name = "Executor"
+                ship4.universe = "Star Wars"
+                
+                try? self.moc.save()
             }
         }
     }
