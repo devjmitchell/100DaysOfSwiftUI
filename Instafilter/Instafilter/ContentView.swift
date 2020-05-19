5a6ec7a326b64a5afa769049ef98138e04c1d46a
Jason Mitchell <devjmitchell@gmail.com>
2020-05-19 08:57:42 -0700

    Day 63 - use twirl filter

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -27,10 +27,11 @@ struct ContentView: View {
         let beginImage = CIImage(image: inputImage)
         
         let context = CIContext()
-        let currentFilter = CIFilter.sepiaTone()
+        guard let currentFilter = CIFilter(name: "CITwirlDistortion") else { return }
         
-        currentFilter.inputImage = beginImage
-        currentFilter.intensity = 1
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        currentFilter.setValue(2000, forKey: kCIInputRadiusKey)
+        currentFilter.setValue(CIVector(x: inputImage.size.width / 2, y: inputImage.size.height / 2), forKey: kCIInputCenterKey)
         
         // get a CIImage from our filter or exit if that fails
         guard let outputImage = currentFilter.outputImage else { return }
