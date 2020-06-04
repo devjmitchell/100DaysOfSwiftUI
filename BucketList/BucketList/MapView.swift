a2d4582db482543121e7971e01b6cd094ff052df
Jason Mitchell <devjmitchell@gmail.com>
2020-06-04 10:10:28 -0700

    Day 69 - Integrating MapKit with SwiftUI

---
+++ BucketList/BucketList/MapView.swift
@@ -0,0 +1,27 @@
+//
+//  MapView.swift
+//  BucketList
+//
+//  Created by Jason Mitchell on 6/4/20.
+//  Copyright © 2020 Jason Mitchell. All rights reserved.
+//
+
+import MapKit
+import SwiftUI
+
+struct MapView: UIViewRepresentable {
+    func makeUIView(context: Context) -> MKMapView {
+        let mapView = MKMapView()
+        return mapView
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
