501673a7c8a2ed77acb0b1915f07589ca56a9630
Jason Mitchell <devjmitchell@gmail.com>
2020-06-11 10:41:52 -0700

    Day 74 - Reading the value of controls

--- Accessibility/Accessibility/ContentView.swift
+++ Accessibility/Accessibility/ContentView.swift
@@ -9,17 +9,18 @@
 import SwiftUI
 
 struct ContentView: View {
+//    @State private var estimate = 25.0
+//
+//    var body: some View {
+//        Slider(value: $estimate, in: 0...50)
+//            .padding()
+//            .accessibility(value: Text("\(Int(estimate))"))
+//    }
+    @State private var rating = 3
+
     var body: some View {
-//        Image(decorative: "character")
-//            .accessibility(hidden: true)
-        VStack {
-            Text("Your score is")
-            Text("1000")
-                .font(.title)
-        }
-//        .accessibilityElement(children: .combine)
-        .accessibilityElement(children: .ignore)
-        .accessibility(label: Text("Your score is 1000"))
+        Stepper("Rate our service: \(rating)/5", value: $rating, in: 1...5)
+            .accessibility(value: Text("\(rating) out of 5"))
     }
 }
 
