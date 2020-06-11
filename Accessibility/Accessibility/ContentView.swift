e20476636f8b99e4baac95725cfbb0396b590e6d
Jason Mitchell <devjmitchell@gmail.com>
2020-06-11 10:21:08 -0700

    Day 74 - create new project "Accessibility"

---
+++ Accessibility/Accessibility/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Accessibility
+//
+//  Created by Jason Mitchell on 6/11/20.
+//  Copyright © 2020 Jason Mitchell. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
