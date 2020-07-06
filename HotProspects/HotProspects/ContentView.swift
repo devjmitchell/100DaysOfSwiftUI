d4a51b628a7429307d6ebed36f9ab91792b294f2
Jason Mitchell <devjmitchell@gmail.com>
2020-07-06 10:10:20 -0700

    Day 80 - Publishing ObservableObject changes

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -8,57 +8,23 @@
 
 import SwiftUI
 
-enum NetworkError: Error {
-    case badURL, requestFailed, unknown
+class DelayedUpdater: ObservableObject {
+    @Published var value = 0
+    
+    init() {
+        for i in 1...10 {
+            DispatchQueue.main.asyncAfter(deadline: .now() + Double(i)) {
+                self.value += 1
+            }
+        }
+    }
 }
 
 struct ContentView: View {
-    @State private var selectedTab = 0
+    @ObservedObject var updater = DelayedUpdater()
     
     var body: some View {
-        Text("Hello, World!")
-            .onAppear {
-                self.fetchData(from: "https://www.apple.com") { result in
-                    switch result {
-                    case .success(let str):
-                        print(str)
-                    case .failure(let error):
-                        switch error {
-                        case .badURL:
-                            print("Bad URL")
-                        case .requestFailed:
-                            print("Network problems")
-                        case .unknown:
-                            print("Unknown error")
-                        }
-                    }
-                }
-        }
-    }
-    
-    func fetchData(from urlString: String, completion: @escaping (Result<String, NetworkError>) -> Void) {
-        // check the URL is OK, otherwise return with a failure
-        guard let url = URL(string: urlString) else {
-            completion(.failure(.badURL))
-            return
-        }
-        
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            // the task has completed – push our work back to the main thread
-            DispatchQueue.main.async {
-                if let data = data {
-                    // success: convert the data to a string and send it back
-                    let stringData = String(decoding: data, as: UTF8.self)
-                    completion(.success(stringData))
-                } else if error != nil {
-                    // any sort of network failure
-                    completion(.failure(.requestFailed))
-                } else {
-                    // this ought not to be possible, yet here we are
-                    completion(.failure(.unknown))
-                }
-            }
-        }.resume()
+        Text("Value is: \(updater.value)")
     }
 }
 
