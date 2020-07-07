5ccbaab63485d3b30a1946e26a407f75213bcb41
Jason Mitchell <devjmitchell@gmail.com>
2020-07-07 10:01:44 -0700

    Day 82 - Building our tab bar

---
+++ HotProspects/HotProspects/MeView.swift
@@ -0,0 +1,21 @@
+//
+//  MeView.swift
+//  HotProspects
+//
+//  Created by Jason Mitchell on 7/7/20.
+//  Copyright © 2020 Jason Mitchell. All rights reserved.
+//
+
+import SwiftUI
+
+struct MeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MeView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeView()
+    }
+}
---
+++ HotProspects/HotProspects/ProspectsView.swift
@@ -0,0 +1,40 @@
+//
+//  ProspectsView.swift
+//  HotProspects
+//
+//  Created by Jason Mitchell on 7/7/20.
+//  Copyright © 2020 Jason Mitchell. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProspectsView: View {
+    enum FilterType {
+        case none, contacted, uncontacted
+    }
+    
+    let filter: FilterType
+    
+    var title: String {
+        switch filter {
+        case .none:
+            return "Everyone"
+        case .contacted:
+            return "Contacted people"
+        case .uncontacted:
+            return "Uncontacted people"
+        }
+    }
+    var body: some View {
+        NavigationView {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .navigationBarTitle(title)
+        }
+    }
+}
+
+struct ProspectsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProspectsView(filter: .none)
+    }
+}
