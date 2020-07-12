b5fbc6b45a6b692628839b837dfac6f2342a554d
Jason Mitchell <devjmitchell@gmail.com>
2020-07-12 16:21:52 -0700

    Day 97 - Creating a secondary view for NavigationView

---
+++ SnowSeeker/SnowSeeker/ResortDetailsView.swift
@@ -0,0 +1,45 @@
+//
+//  ResortDetailsView.swift
+//  SnowSeeker
+//
+//  Created by Jason Mitchell on 7/12/20.
+//  Copyright © 2020 Jason Mitchell. All rights reserved.
+//
+
+import SwiftUI
+
+struct ResortDetailsView: View {
+    let resort: Resort
+    
+//    var size: String {
+//        ["Small", "Average", "Large"][resort.size - 1]
+//    }
+    
+    var size: String {
+        switch resort.size {
+        case 1:
+            return "Small"
+        case 2:
+            return "Average"
+        default:
+            return "Large"
+        }
+    }
+    
+    var price: String {
+        String(repeating: "$", count: resort.price)
+    }
+    
+    var body: some View {
+        VStack {
+            Text("Size: \(size)")
+            Text("Price: \(price)")
+        }
+    }
+}
+
+struct ResortDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ResortDetailsView(resort: Resort.example)
+    }
+}
