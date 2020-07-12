a7152fba65736c3645804b2584b0917fee573029
Jason Mitchell <devjmitchell@gmail.com>
2020-07-12 15:03:46 -0700

    Day 96 - Using groups as transparent layout containers

--- SnowSeeker/SnowSeeker/ContentView.swift
+++ SnowSeeker/SnowSeeker/ContentView.swift
@@ -8,39 +8,59 @@
 
 import SwiftUI
 
-struct User: Identifiable {
-    var id = "Taylor Swift"
-}
-
-struct ContentView: View {
-    @State private var selectedUser: User? = nil
-
+struct UserView: View {
     var body: some View {
-        Text("Hello, World!")
-            .onTapGesture {
-                self.selectedUser = User()
+        Group {
+            Text("Name: Paul")
+            Text("Country: England")
+            Text("Pets: Luna, Arya, and Toby")
         }
-            .alert(item: $selectedUser) { user in
-                Alert(title: Text(user.id))
-            }
     }
 }
 
 //struct ContentView: View {
-//    @State private var selectedUser: User? = nil
-//    @State private var isShowingAlert = false
+//    @State private var layoutVertically = false
 //
 //    var body: some View {
-//        Text("Hello, World!")
-//            .onTapGesture {
-//                self.selectedUser = User()
-//                self.isShowingAlert = true
+//        Group {
+//            if layoutVertically {
+//                VStack {
+//                    UserView()
+//                }
+//            } else {
+//                HStack {
+//                    UserView()
+//                }
 //            }
-//            .alert(isPresented: $isShowingAlert) {
-//                Alert(title: Text(selectedUser!.id))
-//            }
+//        }
+//        .onTapGesture {
+//            self.layoutVertically.toggle()
+//        }
 //    }
 //}
+
+struct ContentView: View {
+    @Environment(\.horizontalSizeClass) var sizeClass
+
+    var body: some View {
+        Group {
+//            if sizeClass == .compact {
+//                VStack {
+//                    UserView()
+//                }
+//            } else {
+//                HStack {
+//                    UserView()
+//                }
+//            }
+            if sizeClass == .compact {
+                VStack(content: UserView.init)
+            } else {
+                HStack(content: UserView.init)
+            }
+        }
+    }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
