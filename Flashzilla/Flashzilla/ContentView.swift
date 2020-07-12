f21dc60267ad2eae8eefc28dd0c84c303f7055d8
Jason Mitchell <devjmitchell@gmail.com>
2020-07-12 11:24:11 -0700

    Day 87 - Supporting specific accessibility needs with SwiftUI

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -8,25 +8,56 @@
 
 import SwiftUI
 
+//struct ContentView: View {
+//    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+//
+//    var body: some View {
+//        HStack {
+//            if differentiateWithoutColor {
+//                Image(systemName: "checkmark.circle")
+//            }
+//
+//            Text("Success")
+//        }
+//        .padding()
+//        .background(differentiateWithoutColor ? Color.black : Color.green)
+//        .foregroundColor(Color.white)
+//        .clipShape(Capsule())
+//    }
+//}
+
+
+//func withOptionalAnimation<Result>(_ animation: Animation? = .default, _ body: () throws -> Result) rethrows -> Result {
+//    if UIAccessibility.isReduceMotionEnabled {
+//        return try body()
+//    } else {
+//        return try withAnimation(animation, body)
+//    }
+//}
+//
+//struct ContentView: View {
+//    @State private var scale: CGFloat = 1
+//
+//    var body: some View {
+//        Text("Hello, World!")
+//            .scaleEffect(scale)
+//            .onTapGesture {
+//                withOptionalAnimation {
+//                    self.scale *= 1.5
+//                }
+//            }
+//    }
+//}
+
+
 struct ContentView: View {
+    @Environment(\.accessibilityReduceTransparency) var reduceTransparency
     var body: some View {
         Text("Hello, World!")
-            .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification)) { _ in
-                print("Moving to the background!")
-        }
-        .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
-            print("Moving back to the foreground!")
-        }
-        .onReceive(NotificationCenter.default.publisher(for: UIApplication.userDidTakeScreenshotNotification)) { _ in
-            print("User took a screenshot!")
-        }
-        
-        /*
-         There are many more... Some others to try are:
-         
-         - UIApplication.significantTimeChangeNotification is called when the user changes their clock or when daylight savings time changes.
-         - UIResponder.keyboardDidShowNotification is called when the keyboard is shown.
-         */
+            .padding()
+            .background(reduceTransparency ? Color.black : Color.black.opacity(0.5))
+            .foregroundColor(Color.white)
+            .clipShape(Capsule())
     }
 }
 
