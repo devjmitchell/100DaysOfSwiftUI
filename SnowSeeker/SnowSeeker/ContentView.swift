ac9c8e621af2324f8238013d9ca9e64adedcd094
Jason Mitchell <devjmitchell@gmail.com>
2020-07-12 15:07:45 -0700

    Day 96 - put project back in starting state to begin the real app

--- SnowSeeker/SnowSeeker/ContentView.swift
+++ SnowSeeker/SnowSeeker/ContentView.swift
@@ -8,57 +8,9 @@
 
 import SwiftUI
 
-struct UserView: View {
-    var body: some View {
-        Group {
-            Text("Name: Paul")
-            Text("Country: England")
-            Text("Pets: Luna, Arya, and Toby")
-        }
-    }
-}
-
-//struct ContentView: View {
-//    @State private var layoutVertically = false
-//
-//    var body: some View {
-//        Group {
-//            if layoutVertically {
-//                VStack {
-//                    UserView()
-//                }
-//            } else {
-//                HStack {
-//                    UserView()
-//                }
-//            }
-//        }
-//        .onTapGesture {
-//            self.layoutVertically.toggle()
-//        }
-//    }
-//}
-
 struct ContentView: View {
-    @Environment(\.horizontalSizeClass) var sizeClass
-
     var body: some View {
-        Group {
-//            if sizeClass == .compact {
-//                VStack {
-//                    UserView()
-//                }
-//            } else {
-//                HStack {
-//                    UserView()
-//                }
-//            }
-            if sizeClass == .compact {
-                VStack(content: UserView.init)
-            } else {
-                HStack(content: UserView.init)
-            }
-        }
+        Text("Hello, World!")
     }
 }
 
