7cf348d83d9d4bc6d8e2923ff07920389988de6f
Jason Mitchell <devjmitchell@gmail.com>
2020-07-12 14:32:06 -0700

    Day 93 - ScrollView effects using GeometryReader

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -9,57 +9,48 @@
 import SwiftUI
 
 //struct ContentView: View {
-//    var body: some View {
-//        VStack {
-//            GeometryReader { geo in
-//                Text("Hello, World!")
-//                    .frame(width: geo.size.width * 0.9, height: 40)
-//                    .background(Color.red)
-//            }
+//    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
 //
-//            Text("More text")
-//                .background(Color.blue)
+//    var body: some View {
+//        GeometryReader { fullView in
+//            ScrollView(.vertical) {
+//                ForEach(0..<50) { index in
+//                    GeometryReader { geo in
+//                        Text("Row #\(index)")
+//                            .font(.title)
+//                            .frame(width: fullView.size.width)
+//                            .background(self.colors[index % 7])
+////                            .rotation3DEffect(.degrees(Double(geo.frame(in: .global).minY) / 5), axis: (x: 0, y: 1, z: 0))
+//                            .rotation3DEffect(.degrees(Double(geo.frame(in: .global).minY - fullView.size.height / 2) / 5), axis: (x: 0, y: 1, z: 0))
+//                    }
+//                    .frame(height: 40)
+//                }
+//            }
 //        }
-//        .background(Color.green)
 //    }
 //}
 
-
-struct OuterView: View {
-    var body: some View {
-        VStack {
-            Text("Top")
-            InnerView()
-                .background(Color.green)
-            Text("Bottom")
-        }
-    }
-}
-
-struct InnerView: View {
-    var body: some View {
-        HStack {
-            Text("Left")
-            GeometryReader { geo in
-                Text("Center")
-                    .background(Color.blue)
-                    .onTapGesture {
-                        print("Global center: \(geo.frame(in: .global).midX) x \(geo.frame(in: .global).midY)")
-                        print("Custom center: \(geo.frame(in: .named("Custom")).midX) x \(geo.frame(in: .named("Custom")).midY)")
-                        print("Local center: \(geo.frame(in: .local).midX) x \(geo.frame(in: .local).midY)")
-                    }
-            }
-            .background(Color.orange)
-            Text("Right")
-        }
-    }
-}
-
 struct ContentView: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+
     var body: some View {
-        OuterView()
-            .background(Color.red)
-            .coordinateSpace(name: "Custom")
+        GeometryReader { fullView in
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    ForEach(0..<50) { index in
+                        GeometryReader { geo in
+                            Rectangle()
+                                .fill(self.colors[index % 7])
+                                .frame(height: 150)
+                                .rotation3DEffect(.degrees(-Double(geo.frame(in: .global).midX - fullView.size.width / 2) / 10), axis: (x: 0, y: 1, z: 0))
+                        }
+                        .frame(width: 150)
+                    }
+                }
+                .padding(.horizontal, (fullView.size.width - 150) / 2)
+            }
+        }
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
