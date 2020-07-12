b3b83d6449f525c3914c11422957ccd3ee0e6694
Jason Mitchell <devjmitchell@gmail.com>
2020-07-12 11:25:28 -0700

    Day 87 - put project back to "clean state" to start building the app

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -8,56 +8,9 @@
 
 import SwiftUI
 
-//struct ContentView: View {
-//    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
-//
-//    var body: some View {
-//        HStack {
-//            if differentiateWithoutColor {
-//                Image(systemName: "checkmark.circle")
-//            }
-//
-//            Text("Success")
-//        }
-//        .padding()
-//        .background(differentiateWithoutColor ? Color.black : Color.green)
-//        .foregroundColor(Color.white)
-//        .clipShape(Capsule())
-//    }
-//}
-
-
-//func withOptionalAnimation<Result>(_ animation: Animation? = .default, _ body: () throws -> Result) rethrows -> Result {
-//    if UIAccessibility.isReduceMotionEnabled {
-//        return try body()
-//    } else {
-//        return try withAnimation(animation, body)
-//    }
-//}
-//
-//struct ContentView: View {
-//    @State private var scale: CGFloat = 1
-//
-//    var body: some View {
-//        Text("Hello, World!")
-//            .scaleEffect(scale)
-//            .onTapGesture {
-//                withOptionalAnimation {
-//                    self.scale *= 1.5
-//                }
-//            }
-//    }
-//}
-
-
 struct ContentView: View {
-    @Environment(\.accessibilityReduceTransparency) var reduceTransparency
     var body: some View {
         Text("Hello, World!")
-            .padding()
-            .background(reduceTransparency ? Color.black : Color.black.opacity(0.5))
-            .foregroundColor(Color.white)
-            .clipShape(Capsule())
     }
 }
 
