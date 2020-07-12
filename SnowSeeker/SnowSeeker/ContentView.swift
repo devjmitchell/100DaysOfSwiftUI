07d2b38825f24f9306658898facac63d2e0fc205
Jason Mitchell <devjmitchell@gmail.com>
2020-07-12 15:00:53 -0700

    Day 96 - Using alert() and sheet() with optionals

--- SnowSeeker/SnowSeeker/ContentView.swift
+++ SnowSeeker/SnowSeeker/ContentView.swift
@@ -8,24 +8,39 @@
 
 import SwiftUI
 
+struct User: Identifiable {
+    var id = "Taylor Swift"
+}
+
 struct ContentView: View {
+    @State private var selectedUser: User? = nil
+
     var body: some View {
-//        NavigationView {
-//            Text("Hello, World!")
-//                .navigationBarTitle("Primary")
-//
-//            Text("Secondary")
-//        }
-        NavigationView {
-            NavigationLink(destination: Text("New secondary")) {
-                Text("Hello, World!")
-            }
-            .navigationBarTitle("Primary")
-            
-            Text("Secondary")
+        Text("Hello, World!")
+            .onTapGesture {
+                self.selectedUser = User()
         }
+            .alert(item: $selectedUser) { user in
+                Alert(title: Text(user.id))
+            }
     }
 }
+
+//struct ContentView: View {
+//    @State private var selectedUser: User? = nil
+//    @State private var isShowingAlert = false
+//
+//    var body: some View {
+//        Text("Hello, World!")
+//            .onTapGesture {
+//                self.selectedUser = User()
+//                self.isShowingAlert = true
+//            }
+//            .alert(isPresented: $isShowingAlert) {
+//                Alert(title: Text(selectedUser!.id))
+//            }
+//    }
+//}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
