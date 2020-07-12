265799cb76ed8f4b34596005bb5be465611c0fc5
Jason Mitchell <devjmitchell@gmail.com>
2020-07-12 14:25:03 -0700

    Day 93 - Understanding frames and coordinates inside GeometryReader

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -10,33 +10,56 @@ import SwiftUI
 
 //struct ContentView: View {
 //    var body: some View {
-//        Text("Hello, world!")
-//            .background(Color.red)
-//            .position(x: 100, y: 100)
+//        VStack {
+//            GeometryReader { geo in
+//                Text("Hello, World!")
+//                    .frame(width: geo.size.width * 0.9, height: 40)
+//                    .background(Color.red)
+//            }
+//
+//            Text("More text")
+//                .background(Color.blue)
+//        }
+//        .background(Color.green)
 //    }
 //}
 
-//struct ContentView: View {
-//    var body: some View {
-//        Text("Hello, world!")
-//            .position(x: 100, y: 100)
-//            .background(Color.red)
-//    }
-//}
 
-//struct ContentView: View {
-//    var body: some View {
-//        Text("Hello, world!")
-//            .offset(x: 100, y: 100)
-//            .background(Color.red)
-//    }
-//}
+struct OuterView: View {
+    var body: some View {
+        VStack {
+            Text("Top")
+            InnerView()
+                .background(Color.green)
+            Text("Bottom")
+        }
+    }
+}
+
+struct InnerView: View {
+    var body: some View {
+        HStack {
+            Text("Left")
+            GeometryReader { geo in
+                Text("Center")
+                    .background(Color.blue)
+                    .onTapGesture {
+                        print("Global center: \(geo.frame(in: .global).midX) x \(geo.frame(in: .global).midY)")
+                        print("Custom center: \(geo.frame(in: .named("Custom")).midX) x \(geo.frame(in: .named("Custom")).midY)")
+                        print("Local center: \(geo.frame(in: .local).midX) x \(geo.frame(in: .local).midY)")
+                    }
+            }
+            .background(Color.orange)
+            Text("Right")
+        }
+    }
+}
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        OuterView()
             .background(Color.red)
-            .offset(x: 100, y: 100)
+            .coordinateSpace(name: "Custom")
     }
 }
 
