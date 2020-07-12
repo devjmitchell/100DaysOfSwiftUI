8ba1d15622cbc73e034a6a66316e546a862f1afd
Jason Mitchell <devjmitchell@gmail.com>
2020-07-12 16:41:55 -0700

    Day 98 - Binding an alert to an optional string

--- SnowSeeker/SnowSeeker/ResortView.swift
+++ SnowSeeker/SnowSeeker/ResortView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct ResortView: View {
     @Environment(\.horizontalSizeClass) var sizeClass
+    @State private var selectedFacility: Facility?
     
     let resort: Resort
     
@@ -43,15 +44,30 @@ struct ResortView: View {
                     Text("Facilities")
                         .font(.headline)
                     
-                    Text(ListFormatter.localizedString(byJoining: resort.facilities))
-                        .padding(.vertical)
+                    HStack {
+                        ForEach(resort.facilityTypes) { facility in
+                            facility.icon
+                                .font(.title)
+                                .onTapGesture {
+                                    self.selectedFacility = facility
+                            }
+                        }
+                    }
+                    .padding(.vertical)
                 }
                 .padding(.horizontal)
             }
         }
         .navigationBarTitle(Text("\(resort.name), \(resort.country)"), displayMode: .inline)
+        .alert(item: $selectedFacility) { facility in
+            facility.alert
+        }
     }
 }
+
+//extension String: Identifiable {
+//    public var id: String { self }
+//}
 
 struct ResortView_Previews: PreviewProvider {
     static var previews: some View {
