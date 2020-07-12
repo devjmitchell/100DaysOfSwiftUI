80ae47dec28f6d2332b6efc2808b1915c95cfbe5
Jason Mitchell <devjmitchell@gmail.com>
2020-07-12 11:02:45 -0700

    Day 86 - Making vibrations with UINotificationFeedbackGenerator and Core Haptics

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -6,152 +6,70 @@
 //  Copyright © 2020 Jason Mitchell. All rights reserved.
 //
 
+import CoreHaptics
 import SwiftUI
 
-//struct ContentView: View {
-//    var body: some View {
-//        Text("Hello, World!")
-//        .onTapGesture(count: 2) {
-//            print("Double tapped!")
-//        }
-//        .onLongPressGesture {
-//            print("Long pressed!")
-//        }
-//        .onLongPressGesture(minimumDuration: 2) {
-//            print("Long pressed!")
-//        }
-//        .onLongPressGesture(minimumDuration: 1, pressing: { inProgress in
-//            print("In progress: \(inProgress)!")
-//        }) {
-//            print("Long pressed!")
-//        }
-//    }
-//}
-
-//struct ContentView: View {
-//    @State private var currentAmount: CGFloat = 0
-//    @State private var finalAmount: CGFloat = 1
-//
-//    var body: some View {
-//        Text("Hello, World!")
-//            .scaleEffect(finalAmount + currentAmount)
-//            .gesture(
-//                MagnificationGesture()
-//                    .onChanged { amount in
-//                        self.currentAmount = amount - 1
-//                    }
-//                    .onEnded { amount in
-//                        self.finalAmount += self.currentAmount
-//                        self.currentAmount = 0
-//                    }
-//            )
-//    }
-//}
-
-//struct ContentView: View {
-//    @State private var currentAmount: Angle = .degrees(0)
-//    @State private var finalAmount: Angle = .degrees(0)
-//
-//    var body: some View {
-//        Text("Hello, World!")
-//            .rotationEffect(currentAmount + finalAmount)
-//            .gesture(
-//                RotationGesture()
-//                    .onChanged { angle in
-//                        self.currentAmount = angle
-//                    }
-//                    .onEnded { angle in
-//                        self.finalAmount += self.currentAmount
-//                        self.currentAmount = .degrees(0)
-//                    }
-//            )
-//    }
-//}
-
-//struct ContentView: View {
-//    var body: some View {
-//        VStack {
-//            Text("Hello, World!")
-//                .onTapGesture {
-//                    print("Text tapped")
-//                }
-//        }
-//        .onTapGesture {
-//            print("VStack tapped")
-//        }
-//    }
-//}
-
-//struct ContentView: View {
-//    var body: some View {
-//        VStack {
-//            Text("Hello, World!")
-//                .onTapGesture {
-//                    print("Text tapped")
-//                }
-//        }
-//        .highPriorityGesture(
-//            TapGesture()
-//                .onEnded { _ in
-//                    print("VStack tapped")
-//                }
-//        )
-//    }
-//}
-
-//struct ContentView: View {
-//    var body: some View {
-//        VStack {
-//            Text("Hello, World!")
-//                .onTapGesture {
-//                    print("Text tapped")
-//                }
-//        }
-//        .simultaneousGesture(
-//            TapGesture()
-//                .onEnded { _ in
-//                    print("VStack tapped")
-//                }
-//        )
-//    }
-//}
-
 struct ContentView: View {
-    // how far the circle has been dragged
-    @State private var offset = CGSize.zero
-
-    // whether it is currently being dragged or not
-    @State private var isDragging = false
-
+    @State private var engine: CHHapticEngine?
+    
     var body: some View {
-        // a drag gesture that updates offset and isDragging as it moves around
-        let dragGesture = DragGesture()
-            .onChanged { value in self.offset = value.translation }
-            .onEnded { _ in
-                withAnimation {
-                    self.offset = .zero
-                    self.isDragging = false
-                }
-            }
+//        Text("Hello, World!")
+//            .onTapGesture(perform: simpleSuccess)
+        
+        Text("Hello, World!")
+            .onAppear(perform: prepareHaptics)
+            .onTapGesture(perform: complexSuccess)
+    }
+    
+//    func simpleSuccess() {
+//        let generator = UINotificationFeedbackGenerator()
+//        generator.notificationOccurred(.success)
+//    }
+    
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
 
-        // a long press gesture that enables isDragging
-        let pressGesture = LongPressGesture()
-            .onEnded { value in
-                withAnimation {
-                    self.isDragging = true
-                }
-            }
+        do {
+            self.engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("There was an error creating the engine: \(error.localizedDescription)")
+        }
+    }
+    
+    func complexSuccess() {
+        // make sure that the device supports haptics
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        var events = [CHHapticEvent]()
 
-        // a combined gesture that forces the user to long press then drag
-        let combined = pressGesture.sequenced(before: dragGesture)
+        // create one intense, sharp tap
+//        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+//        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
+//        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
+//        events.append(event)
+        
+        for i in stride(from: 0, to: 1, by: 0.1) {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: i)
+            events.append(event)
+        }
 
-        // a 64x64 circle that scales up when it's dragged, sets its offset to whatever we had back from the drag gesture, and uses our combined gesture
-        return Circle()
-            .fill(Color.red)
-            .frame(width: 64, height: 64)
-            .scaleEffect(isDragging ? 1.5 : 1)
-            .offset(offset)
-            .gesture(combined)
+        for i in stride(from: 0, to: 1, by: 0.1) {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(1 - i))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(1 - i))
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 1 + i)
+            events.append(event)
+        }
+
+        // convert those events into a pattern and play it immediately
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play pattern: \(error.localizedDescription).")
+        }
     }
 }
 
